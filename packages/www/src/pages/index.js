c2b4216ec3ca7649852cb0e5567cd76efbcce58a
Mike Falanga <m.falanga@gmail.com>
2021-03-01 22:31:07 -0500

    Install Theme UI

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-export default (props) => (
-  <div>
-    <h1>Our Site</h1>
-  </div>
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Button, Container, Flex, Heading } from 'theme-ui';
+
+const Main = (props) => (
+  <Container>
+    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+      <Heading as="h1">Get Stuff Done</Heading>
+      <Button sx={{ marginTop: 2 }} onClick={() => alert('clicked')}>
+        Log In
+      </Button>
+    </Flex>
+  </Container>
 );
+
+export default Main;
