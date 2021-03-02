35cc9d29408d82c75d76927af34db87b22918308
Mike Falanga <m.falanga@gmail.com>
2021-03-01 22:44:56 -0500

    Add authentication

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,16 +1,24 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Button, Container, Flex, Heading } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
+import { useEffect } from 'react';
 
-const Main = (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button sx={{ marginTop: 2 }} onClick={() => alert('clicked')}>
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-);
+const Main = (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button sx={{ marginTop: 2 }} onClick={() => netlifyIdentity.open()}>
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
 
 export default Main;
